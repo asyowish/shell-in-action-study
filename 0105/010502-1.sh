c60d22478ad54b1b83ae9437326741184186ee2f
Jedi Chou <skyzhx@163.com>
2016-11-02 20:56:59 +0800

    提交-重定向的2个脚本

---
+++ 0105/010502-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Jedi: Use file redirect to create a text file.
+# Packt Linux shell cookbook, 1st, p29
+echo "This is a sample text 1" > temp.txt
---
+++ 0105/010502-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Jedi: use redirection to append line to text file.
+echo "This is sample text 2" >> temp.txt
